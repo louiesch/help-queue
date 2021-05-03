6ff59124cba68ad4adb8dc46a8e322d8991f998e
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-02 21:22:38 -0700

    add action test for toggle form

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,10 +1,17 @@
 import * as actions from './../../actions';
 
 describe('help queue actions', () => {
+
   it('deleteTicket should create DELETE_TICKET action', () => {
     expect(actions.deleteTicket(1)).toEqual({
       type: 'DELETE_TICKET',
       id: 1
+    });
+  });
+
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
     });
   });
 });
